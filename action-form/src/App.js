90538b2058c108855f2b8663b3e1c98ce55ea4b8
mauricekollewe <kollewem.35@gmail.com>
2022-05-19 09:35:03 +0100

    update app

--- action-form/src/App.js
+++ action-form/src/App.js
@@ -1,11 +1,26 @@
+import { Card } from 'react-bootstrap'
 
 const App = () => {
+
   return (
-    <div className="App">
-        <p>
-          Welcome Home!
-        </p>
-    </div>
+    <section className="container">
+      <>
+        <Card>
+          <Card.Body>
+            <Card.Text>
+              Some data.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Text>
+              Some data.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </>
+    </section>
   );
 }
 
