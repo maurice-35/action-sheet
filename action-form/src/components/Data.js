860e1fa7228568ed4f048f781fffcefc48727417
mauricekollewe <kollewem.35@gmail.com>
2022-05-19 13:58:42 +0100

    create contactInfo & data

--- action-form/src/components/Data.js
+++ action-form/src/components/Data.js
@@ -1,25 +1,30 @@
 export const Contacts = [(
-	{ name: 'James Sam',
+	{
+		name: 'James Sam',
 		phone: '09877446574',
 		email: 'james@email.com',
 		category: 'friend',
 	},
-	{ name: 'Andrew Nap',
+	{
+		name: 'Andrew Nap',
 		phone: '19876446574',
 		email: 'andrew@email.com',
 		category: 'family',
 	},
-	{ name: 'Peter Look',
+	{
+		name: 'Peter Look',
 		phone: '29877446579',
 		email: 'peter@email.com',
 		category: 'work',
 	},
-	{ name: 'Sean Cook',
+	{
+		name: 'Sean Cook',
 		phone: '00977446574',
 		email: 'sean@email.com',
 		category: 'family',
 	},
-	{ name: 'Martin Everest',
+	{
+		name: 'Martin Everest',
 		phone: '11877441174',
 		email: 'martin@email.com',
 		category: 'work',
--- action-form/src/components/contactInfo.js
+++ action-form/src/components/contactInfo.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 export const RenderContactInfo = ({ name, phone, email, category }) => {
-  return (
-    <div>
-      <div>{name}</div>
-      <div>{phone}</div>
-      <div>{email}</div>
+	return (
+		<div>
+			<div>{name}</div>
+			<div>{phone}</div>
+			<div>{email}</div>
 			<div>{category}</div>
-    </div>
-  );
+		</div>
+	);
 }
